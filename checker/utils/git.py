d10dbe3396b79e1c4a6aa6dd7ad95881a51950c1
Konstantin Chernyshev <38007247+k4black@users.noreply.github.com>
2022-09-10 18:34:22 +0200

    fix: Export fixes fixes (#11)
    * fix: refactor git setup for export
    
    * fix: refactor git setup for export
    
    * style: fix flake8 style
    
    * style: fix mypy style

--- checker/utils/git.py
+++ checker/utils/git.py
@@ -17,41 +17,58 @@ def setup_repo_in_dir(
         branch: str = DEFAULT_BRANCH,
 ) -> None:
     remote_repo_url = remote_repo_url.replace('https://', '').replace('http://', '').replace('.git', '')
+    remote_repo_url = f'https://{service_username}:{service_token}@{remote_repo_url}.git'
     print_info(f'remote_repo_url {remote_repo_url}', color='grey')
 
     shutil.rmtree(repo_dir)
     repo_dir.mkdir()
 
     print_info('* git clone...')
-    t = subprocess.run(
-        f'git clone --depth=1 --branch={branch} https://{service_username}:{service_token}@{remote_repo_url}.git ./',
+    r = subprocess.run(
+        f'git clone --depth=1 --branch={branch} {remote_repo_url} ./',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
+        check=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
+    if r.returncode != 0:
+        print_info('empty repo, init it first')
+        r = subprocess.run(
+            f'git init --initial-branch {branch} && git remote add origin {remote_repo_url} && git ch',
+            encoding='utf-8',
+            stdout=subprocess.PIPE,
+            shell=True,
+            cwd=repo_dir,
+        )
+        print_info(r.stdout, color='grey')
 
     print_info('* git remote...')
-    t = subprocess.run(
+    r = subprocess.run(
         'git remote -v',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
+        check=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
 
     print_info('* git config set...')
-    t = subprocess.run(
+    r = subprocess.run(
         f'git config --local user.email "{git_user_email}" && '
         f'git config --local user.name "{git_user_name}"',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
+        check=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
 
 
 def commit_push_all_repo(
@@ -60,43 +77,48 @@ def commit_push_all_repo(
         message: str = 'Export public files',
 ) -> None:
     print_info('* git status...')
-    t = subprocess.run(
+    r = subprocess.run(
         'git status',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
+        check=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
 
     print_info('* adding files...')
-    t = subprocess.run(
+    r = subprocess.run(
         'git add .',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
 
     print_info('* committing...')
-    t = subprocess.run(
+    r = subprocess.run(
         f'git commit --all -m "{message}"',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
 
     print_info('* git pushing...')
-    t = subprocess.run(
+    r = subprocess.run(
         f'git push -o ci.skip origin {branch}',
         encoding='utf-8',
         stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         shell=True,
         cwd=repo_dir,
-    ).stdout
-    print_info(t, color='grey')
+    )
+    print_info(r.stdout, color='grey')
 
     print_info('Done.')
