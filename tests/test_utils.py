74c19c7f9e9619b08b4e5f0844b261bc17635197
Konstantin Chernyshev <k4black@ya.ru>
2023-12-14 11:59:42 +0100

    test: add print basic tests

---
+++ tests/test_utils.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import pytest
+
+from checker.utils import print_info, print_separator, print_header_info
+
+
+class TestPrint:
+    # TODO: test print colors etc
+
+    def test_print_info(self, capsys: pytest.CaptureFixture):
+        print_info('123')
+
+        captured = capsys.readouterr()
+        assert captured.err == '123\n'
+
+    def test_print_separator(self, capsys: pytest.CaptureFixture):
+        print_separator('*', string_length=10)
+
+        captured = capsys.readouterr()
+        assert '**********' in captured.err
+
+    def test_print_header_info(self, capsys: pytest.CaptureFixture):
+        print_header_info('123', string_length=10)
+
+        captured = capsys.readouterr()
+        assert '123' in captured.err
+        assert '++++++++++' in captured.err
