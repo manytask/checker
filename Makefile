b3c24c237c5a679b0d06e99d72b91da99d5e203f
Konstantin Chernyshev <k4black@ya.ru>
2023-12-15 11:06:15 +0100

    chore: add makefile for testing and linting

---
+++ Makefile
@@ -0,0 +1,51 @@
+#.RECIPEPREFIX = >>
+# Default task to run when no task is specified
+all: help
+
+# Help task to display callable targets
+help:
+	@echo "Makefile commands:"
+	@echo "test-unit        - Run unit tests with pytest"
+	@echo "test-integration - Run integration tests with pytest"
+	@echo "test-docstests   - Run doctests with pytest"
+	@echo "test             - Run all tests with pytest"
+	@echo "lint             - Lint and typecheck the code"
+	@echo "format           - Format the code with black"
+	@echo "docs             - Build the documentation"
+	@echo "help             - Display this help"
+
+# Run unit tests only
+test-unit:
+	@echo "[make] Running unit tests..."
+	pytest --skip-integration --skip-doctest
+
+# Run integration tests only
+test-integration:
+	@echo "[make] Running integration tests..."
+	pytest --skip-unit --skip-doctest
+
+# Run doctests only
+test-docstests:
+	@echo "[make] Running doctests..."
+	pytest --skip-unit --skip-integration
+
+# Run all tests
+test:
+	@echo "[make] Running unit and integration tests..."
+	pytest
+
+# Lint and typecheck the code
+lint:
+	@echo "[make] Linting and typechecking the code..."
+	ruff check checker tests
+	mypy checker tests
+	black --check checker tests
+	isort --check-only checker tests
+
+# Format the code with black and isort
+format:
+	@echo "[make] Formatting the code..."
+	black checker tests
+	isort checker tests
+
+.PHONY: all help test-unit test-integration test-docstests test lint format
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,28 +3,50 @@ import pytest
 
 def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption(
-        "--integration",
+        "--skip-integration",
         action="store_true",
-        dest="integration",
+        dest="skip_integration",
         default=False,
-        help="enable integration tests",
+        help="skip integration tests",
+    )
+    parser.addoption(
+        "--skip-unit",
+        action="store_true",
+        dest="skip_unit",
+        default=False,
+        help="skip unit tests",
+    )
+    parser.addoption(
+        "--skip-doctest",
+        action="store_true",
+        dest="skip_unit",
+        default=False,
+        help="skip doctest",
     )
 
 
 def pytest_configure(config: pytest.Config) -> None:
     config.addinivalue_line("markers", "integration: mark test as integration test")
 
+    # Add --doctest-modules by default if --skip-doctest is not set
+    if not config.getoption("--skip-doctest"):
+        config.addinivalue_line("addopts", "--doctest-modules")
+
 
 def pytest_collection_modifyitems(
     config: pytest.Config,
     items: list[pytest.Item],
 ) -> None:
-    if config.getoption("--integration"):
-        # --integration given in cli: do not skip integration tests
-        return
-
-    skip_integration = pytest.mark.skip(reason="need --integration option to run")
+    skip_integration = pytest.mark.skip(reason="--skip-integration option was provided")
+    skip_unit = pytest.mark.skip(reason="--skip-unit option was provided")
+    skip_doctest = pytest.mark.skip(reason="--skip-doctest option was provided")
 
     for item in items:
-        if "integration" in item.keywords:
-            item.add_marker(skip_integration)
+        if isinstance(item, pytest.DoctestItem):
+            item.add_marker(skip_doctest)
+        elif "integration" in item.keywords:
+            if config.getoption("--skip-integration"):
+                item.add_marker(skip_integration)
+        else:
+            if config.getoption("--skip-unit"):
+                item.add_marker(skip_unit)
