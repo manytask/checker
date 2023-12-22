096486456d5a5a2328c8d3dea89855708ec6d3e7
fortrest <ars.sobolev@gmail.com>
2023-12-22 22:09:34 +0600

    add test for files collection

--- tests/plugins/test_manytask.py
+++ tests/plugins/test_manytask.py
@@ -1,18 +1,27 @@
 from __future__ import annotations
 
-from typing import Any
 from datetime import datetime
+from tempfile import TemporaryDirectory, NamedTemporaryFile
+from typing import Any
+from unittest import mock
+from os.path import basename
 
-from pytest_mock import MockFixture
-import requests
 import pytest
 from pydantic import ValidationError
 
-from checker.exceptions import PluginExecutionFailed
 from checker.plugins.manytask import ManytaskPlugin
 
 
 class TestManytaskPlugin:
+    @staticmethod
+    def get_sample_args() -> ManytaskPlugin.Args:
+        return ManytaskPlugin.Args(username='user1',
+                                   task_name='task1',
+                                   score=0.5,
+                                   report_url='https://example.com',
+                                   tester_token='token1',
+                                   check_deadline=True)
+
     @pytest.mark.parametrize(
         "parameters, expected_exception",
         [
@@ -22,7 +31,7 @@ class TestManytaskPlugin:
                  'score': 0.5,
                  'report_url': 'https://example.com',
                  'tester_token': 'token1',
-                 'check_deadline': True
+                 'check_deadline': True,
              }, None),
             ({
                  'origin': 'test/',
@@ -89,3 +98,46 @@ class TestManytaskPlugin:
                 ManytaskPlugin.Args(**parameters)
         else:
             ManytaskPlugin.Args(**parameters)
+
+    @pytest.mark.parametrize(
+        "extensions_to_create, patterns_to_take, taken_files_num",
+        [
+            (['.py', '.yml', '.txt'],
+             ['*'],
+             3),
+            (['.py', '.yml', '.txt'],
+             ['*.py'],
+             1),
+            (['.py', '.yml', '.py', '.yml', '.txt'],
+             ['*.py', '*.yml'],
+             4),
+            (['.py', '.yml', '.txt'],
+             ['*.not'],
+             0)
+        ]
+    )
+    def test_collect_files_to_send(self, patterns_to_take: list[str], extensions_to_create: list[str],
+                                   taken_files_num: int) -> None:
+        args = self.get_sample_args()
+        args.patterns = patterns_to_take
+
+        with TemporaryDirectory() as tdir:
+            args.origin = tdir
+            tempfiles = []
+            expected_filenames = []
+
+            for extension in extensions_to_create:
+                ntfile = NamedTemporaryFile(dir=tdir, suffix=extension)
+                tempfiles.append(ntfile)
+                if f'*{extension}' in patterns_to_take or '*' in patterns_to_take:
+                    expected_filenames.append(basename(tempfiles[-1].name))
+
+            with mock.patch("builtins.open", mock.mock_open(read_data=b"File content")) as mock_open_file:
+                result = ManytaskPlugin._collect_files_to_send(args)
+
+                assert result is not None, 'Didn\'t collect files'
+                assert len(result) == taken_files_num, 'Wrong file quantity are collected'
+                assert sorted(result.keys()) == sorted(expected_filenames), 'Wrong files are collected'
+
+                if taken_files_num:
+                    mock_open_file.assert_called_with(mock.ANY, "rb")
