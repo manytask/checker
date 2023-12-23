8f53b790cb9106166330e17eb708cf3f90dc7f61
fortrest <ars.sobolev@gmail.com>
2023-12-23 18:02:02 +0600

    fix output for plugin (now not using logging, but properly outputted in run)

--- checker/plugins/manytask.py
+++ checker/plugins/manytask.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import json
-import logging
 from datetime import datetime
 from pathlib import Path
 from typing import Optional, IO, Any
@@ -19,10 +18,11 @@ class ManytaskPlugin(PluginABC):
     """Given score report it to the manytask."""
 
     name = "report_score_manytask"
+    _output: list[str]
 
     class Args(PluginABC.Args):
         origin: Optional[str] = None  # as pydantic does not support | in older python versions
-        patterns: Optional[list[str]] = None  # as pydantic does not support | in older python versions
+        patterns: list[str] = ['*']
         username: str
         task_name: str
         score: float  # TODO: validate score is in [0, 1] (bonus score im higher than 1)
@@ -34,6 +34,7 @@ class ManytaskPlugin(PluginABC):
     def _run(self, args: Args, *, verbose: bool = False) -> PluginOutput:
         # TODO: check on requests 2.0.0
         time_isoformat = '%Y-%m-%dT%H:%M:%S.%f%:z'
+        self._output = []
         # Do not expose token in logs.
         data = {
             'token': args.tester_token,
@@ -44,18 +45,20 @@ class ManytaskPlugin(PluginABC):
             'submit_time': args.send_time.strftime(time_isoformat)
         }
 
-        files = self._collect_files_to_send(args)
+        files = None
+        if args.origin is not None:
+            files = self._collect_files_to_send(args.origin, args.patterns)
+
         if verbose:
-            logging.info(files)
+            self._output.append(str(files))
 
         response = self._post_with_retries(args.report_url, data, files)
 
         try:
             result = response.json()
-            return PluginOutput(
-                f"Report for task '{args.task_name}' for user '{args.username}', "
-                f"requested score: {args.score}, result score: {result['score']}"
-            )
+            self._output.append(f"Report for task '{args.task_name}' for user '{args.username}', "
+                                f"requested score: {args.score}, result score: {result['score']}")
+            return PluginOutput(output='\n'.join(self._output))
         except (json.JSONDecodeError, KeyError):
             raise PluginExecutionFailed('Unable to decode response')
 
@@ -79,14 +82,13 @@ class ManytaskPlugin(PluginABC):
         return response
 
     @staticmethod
-    def _collect_files_to_send(args: Args) -> dict[str, tuple[str, IO[bytes]]] | None:
-        if args.origin is not None and args.patterns:
-            source_dir = Path(args.origin)
-            return {
-                path.name: (str(path.relative_to(source_dir)), open(path, 'rb'))
-                for pattern in args.patterns
-                for path in source_dir.glob(pattern)
-                if path.is_file()
-            }
+    def _collect_files_to_send(origin: str, patterns: list[str]) -> dict[str, tuple[str, IO[bytes]]]:
+        source_dir = Path(origin)
+        return {
+            path.name: (str(path.relative_to(source_dir)), open(path, 'rb'))
+            for pattern in patterns
+            for path in source_dir.glob(pattern)
+            if path.is_file()
+        }
 
         return None
