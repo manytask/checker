870a6edb83253212706c111d2c569d4f44511107
Dmitriy Lukashevich <uniqum@yandex-team.ru>
2023-12-25 20:53:42 +0300

    - add custom Args for SafeRunPlugin - add SafeRunPlugin desctiption - format code with Black - add more test cases - some other minor fixes

--- checker/plugins/firejail.py
+++ checker/plugins/firejail.py
@@ -1,39 +1,58 @@
 from __future__ import annotations
 
 import os
+from pathlib import Path
 
 from .scripts import RunScriptPlugin
 
 
-PATH = 'PATH'
-PYTHONPATH = 'PYTHONPATH'
+HOME_PATH = str(Path.home())
 
 
 class SafeRunScriptPlugin(RunScriptPlugin):
-    """Plugin for running scripts in safe mode."""
+    """Wrapper over RunScriptPlugin to run students scripts safety.
+    Plugin uses Firejail tool to create sandbox for the running process.
+    He allows hide environment variables and control access to network and file system.
+    """
 
     name = "safe_run_script"
 
-    Args = RunScriptPlugin.Args
+    class Args(RunScriptPlugin.Args):
+        allow_envs: set[str] = set()
+        lock_network: bool = True
+        allow_paths: set[str] = set()
 
-    # TODO(uniqum): at the moment "--queit" option of firejail may stil put extra strings in the output
+    # TODO: at the moment "--queit" option of firejail may stil put extra strings in the output
     def _run(self, args: Args, *, verbose: bool = False) -> str:
-        whitelist_dir = args.origin if not args.origin.startswith('/tmp') else '~/tmp'
-        command = ['firejail',
-                   '--quiet',
-                   '--noprofile',
-                   # lock network access
-                   '--net=none',
-                   # allow access to origin dir
-                   f'--whitelist={whitelist_dir}',
-                   # hide all environment variables
-                   'env -i',
-                   # copy PATH
-                   f'{PATH}={os.environ.get(PATH, "")}',
-                   # cope PYTHONPATH
-                   f'{PYTHONPATH}={os.environ.get(PYTHONPATH, "")}',
-                   args.script if isinstance(args.script, str) else ' '.join(args.script),
-                   ]
-        run_args = RunScriptPlugin.Args(origin=args.origin, script=' '.join(command), timeout=args.timeout)
+        command = ["firejail", "--quiet", "--noprofile"]
+        # lock network access
+        if args.lock_network:
+            command.append("--net=none")
+        # collect all allow paths
+        allow_paths = args.allow_paths.copy()
+        allow_paths.add(args.origin)
+        # a bit tricky but if paths is only /tmp add ~/tmp instead of it
+        if "/tmp" in allow_paths and len(allow_paths) == 1:
+            allow_paths.add("~/tmp")
+        # remove /tmp from paths as it causes error inside of Firejail
+        if "/tmp" in allow_paths:
+            allow_paths.remove("/tmp")
+        # replace ~ by the full home path
+        for path in allow_paths:
+            full_path = path if not path.startswith("~") else HOME_PATH + path[1:]
+            # allow access to origin dir
+            command.append(f"--whitelist={full_path}")
+        # hide all environment variables
+        command.append("env -i")
+        for env in args.allow_envs:
+            command.append(f'{env}="{os.environ.get(env, "")}"')
+        if isinstance(args.script, str):
+            command.append(args.script)
+        else:
+            assert isinstance(args.script, list)
+            command.extend(args.script)
+        run_args = RunScriptPlugin.Args(
+            origin=args.origin, script=" ".join(command), timeout=args.timeout
+        )
 
         return super()._run(args=run_args, verbose=verbose)
--- tests/plugins/test_firejail.py
+++ tests/plugins/test_firejail.py
@@ -7,38 +7,47 @@ from typing import Any
 import pytest
 from pydantic import ValidationError
 
-from checker.plugins.firejail import PATH
-from checker.plugins.firejail import PYTHONPATH
 from checker.plugins.firejail import SafeRunScriptPlugin
 from checker.exceptions import PluginExecutionFailed
 
 
 PATTERN_ENV = re.compile(r"(?P<name>\S+)=.*")
+PATH = "PATH"
+PYTHONPATH = "PYTHONPATH"
 
 
 class TestSafeRunScriptPlugin:
-
-    @pytest.mark.parametrize("parameters, expected_exception", [
-        ({"origin": "/tmp/123", "script": "echo Hello"}, None),
-        ({"origin": "/tmp/123", "script": 123}, ValidationError),
-        ({"origin": "/tmp/123", "script": ["echo", "Hello"]}, None),
-        ({"origin": "/tmp/123", "script": "echo Hello", "timeout": 10}, None),
-    ])
-    def test_plugin_args(self, parameters: dict[str, Any], expected_exception: Exception | None) -> None:
+    @pytest.mark.parametrize(
+        "parameters, expected_exception",
+        [
+            ({"origin": "/tmp/123", "script": "echo Hello"}, None),
+            ({"origin": "/tmp/123", "script": 123}, ValidationError),
+            ({"origin": "/tmp/123", "script": ["echo", "Hello"]}, None),
+            ({"origin": "/tmp/123", "script": "echo Hello", "timeout": 10}, None),
+        ],
+    )
+    def test_plugin_args(
+        self, parameters: dict[str, Any], expected_exception: Exception | None
+    ) -> None:
         if expected_exception:
             with pytest.raises(expected_exception):
                 SafeRunScriptPlugin.Args(**parameters)
         else:
             SafeRunScriptPlugin.Args(**parameters)
 
-    @pytest.mark.parametrize("script, output, expected_exception", [
-        ("echo Hello", "Hello", None),
-        ("sleep 0.1", "", None),
-        ("true", "", None),
-        ("false", "", PluginExecutionFailed),
-        ("echo Hello && false", "Hello", PluginExecutionFailed),
-    ])
-    def test_run_script(self, script: str, output: str, expected_exception: Exception | None) -> None:
+    @pytest.mark.parametrize(
+        "script, output, expected_exception",
+        [
+            ("echo Hello", "Hello", None),
+            ("sleep 0.1", "", None),
+            ("true", "", None),
+            ("false", "", PluginExecutionFailed),
+            ("echo Hello && false", "Hello", PluginExecutionFailed),
+        ],
+    )
+    def test_run_script(
+        self, script: str, output: str, expected_exception: Exception | None
+    ) -> None:
         plugin = SafeRunScriptPlugin()
         args = SafeRunScriptPlugin.Args(origin="/tmp", script=script)
 
@@ -50,13 +59,18 @@ class TestSafeRunScriptPlugin:
             res = plugin._run(args)
             assert res.output.strip() == output
 
-    @pytest.mark.parametrize("script, timeout, expected_exception", [
-        ("echo Hello", 10, None),
-        ("sleep 0.5", 1, None),
-        ("sleep 1", None, None),
-        ("sleep 2", 1, PluginExecutionFailed),
-    ])
-    def test_timeout(self, script: str, timeout: float, expected_exception: Exception | None) -> None:
+    @pytest.mark.parametrize(
+        "script, timeout, expected_exception",
+        [
+            ("echo Hello", 10, None),
+            ("sleep 0.5", 1, None),
+            ("sleep 1", None, None),
+            ("sleep 2", 1, PluginExecutionFailed),
+        ],
+    )
+    def test_timeout(
+        self, script: str, timeout: float, expected_exception: Exception | None
+    ) -> None:
         # TODO: check if timeout float
         plugin = SafeRunScriptPlugin()
         args = SafeRunScriptPlugin.Args(origin="/tmp", script=script, timeout=timeout)
@@ -67,9 +81,19 @@ class TestSafeRunScriptPlugin:
         else:
             plugin._run(args)
 
-    def test_hide_evns(self) -> None:
+    @pytest.mark.parametrize(
+        "allow_envs",
+        [
+            ([]),
+            ([PATH]),
+            ([PATH, PYTHONPATH]),
+        ],
+    )
+    def test_hide_evns(self, allow_envs: list[str]) -> None:
         plugin = SafeRunScriptPlugin()
-        args = SafeRunScriptPlugin.Args(origin="/tmp", script="printenv")
+        args = SafeRunScriptPlugin.Args(
+            origin="/tmp", script="printenv", allow_envs=allow_envs
+        )
 
         res_lines = [line.strip() for line in plugin._run(args).output.splitlines()]
         envs: list[str] = []
@@ -79,45 +103,94 @@ class TestSafeRunScriptPlugin:
                 envs.append(match.group("name"))
 
         # check if environment has only two items: PATH and PYTHONPATH
-        assert len(envs) == 2
-        assert PATH in envs
-        assert PYTHONPATH in envs
+        assert len(envs) == len(allow_envs)
+        for env in envs:
+            assert env in allow_envs
 
-    @pytest.mark.parametrize("query", [
-        ("curl -i -X GET https://www.example.org"),
-        ("curl --user daniel:secret ftp://example.com/download"),
-    ])
-    def test_network_access(self, query: str) -> None:
+    @pytest.mark.parametrize(
+        "query",
+        [
+            ("curl -i -X GET https://www.example.org"),
+            ("curl --user daniel:secret ftp://example.com/download"),
+        ],
+    )
+    def test_allow_network_access(self, query: str) -> None:
+        # TODO: have to mock network and check if lock_network = False allows access to the Internet
         plugin = SafeRunScriptPlugin()
-        args = SafeRunScriptPlugin.Args(origin="/tmp", script=query)
+        args = SafeRunScriptPlugin.Args(origin="/tmp", script=query, lock_network=True)
 
         with pytest.raises(PluginExecutionFailed):
             plugin._run(args)
 
-    @pytest.mark.parametrize("origin, access_file, expected_exception", [
-        ("/tmp", "/tmp/tmp.txt", None),
-        ("/tmp", Path.home().joinpath("tmp", "tmp.txt"), None),  # this is a trick!!! origin /tmp is replaced by ~/tmp
-        ("/tmp", Path.home().joinpath("tmp.txt"), PluginExecutionFailed),
-        ("/tmp", Path.home().joinpath("not_tmp", "tmp.txt"), PluginExecutionFailed),
-        (Path.home(), Path.home().joinpath("tmp.txt"), None),
-        (Path.home(), Path.home().joinpath("tmp", "tmp.txt"), None),
-        (Path.home(), Path.home().joinpath("not_tmp", "tmp.txt"), None),
-        (Path.home().joinpath("tmp"), Path.home().joinpath("tmp", "tmp.txt"), None),
-        (Path.home().joinpath("tmp"), Path.home().joinpath("tmp.txt"), PluginExecutionFailed),
-        (Path.home().joinpath("tmp"), Path.home().joinpath("not_tmp", "tmp.txt"), PluginExecutionFailed),
-    ])
-    def test_file_system_access(self,
-                                origin: Path | str,
-                                access_file: Path | str,
-                                expected_exception: Exception | None) -> None:
-        access_file_path = Path(access_file)
+    @pytest.mark.parametrize(
+        "origin, allow_paths, access_file, expected_exception",
+        [
+            ("/tmp", [], Path("/tmp/tmp.txt"), None),
+            (
+                "/tmp",
+                [],
+                Path.home().joinpath("tmp", "tmp.txt"),
+                None,
+            ),  # this is a trick!!! origin /tmp is replaced by ~/tmp
+            ("/tmp", [], Path.home().joinpath("tmp.txt"), PluginExecutionFailed),
+            ("/tmp", ["~"], Path.home().joinpath("tmp.txt"), None),
+            ("~", [], Path.home().joinpath("tmp.txt"), None),
+            (
+                "/tmp",
+                [],
+                Path.home().joinpath("not_tmp", "tmp.txt"),
+                PluginExecutionFailed,
+            ),
+            ("/tmp", ["~/not_tmp"], Path.home().joinpath("not_tmp", "tmp.txt"), None),
+            ("~/not_tmp", [], Path.home().joinpath("not_tmp", "tmp.txt"), None),
+            ("~", [], Path.home().joinpath("tmp.txt"), None),
+            ("~/not_tmp", [], Path.home().joinpath("tmp.txt"), PluginExecutionFailed),
+            ("~/not_tmp", ["~"], Path.home().joinpath("tmp.txt"), None),
+            ("~", [], Path.home().joinpath("tmp", "tmp.txt"), None),
+            ("~/not_tmp", ["~"], Path.home().joinpath("tmp", "tmp.txt"), None),
+            ("~", [], Path.home().joinpath("not_tmp", "tmp.txt"), None),
+            ("~/tmp", [], Path.home().joinpath("tmp", "tmp.txt"), None),
+            ("~/not_tmp", ["~/tmp"], Path.home().joinpath("tmp", "tmp.txt"), None),
+            ("~/tmp", [], Path.home().joinpath("tmp.txt"), PluginExecutionFailed),
+            (
+                "~/not_tmp",
+                ["~/tmp"],
+                Path.home().joinpath("tmp.txt"),
+                PluginExecutionFailed,
+            ),
+            (
+                "~/tmp",
+                [],
+                Path.home().joinpath("not_tmp", "tmp.txt"),
+                PluginExecutionFailed,
+            ),
+            (
+                "/tmp",
+                ["~/tmp"],
+                Path.home().joinpath("not_tmp", "tmp.txt"),
+                PluginExecutionFailed,
+            ),
+        ],
+    )
+    def test_file_system_access(
+        self,
+        origin: str,
+        allow_paths: list[str],
+        access_file: Path,
+        expected_exception: Exception | None,
+    ) -> None:
+        access_file_path = access_file
         access_file_path.parent.mkdir(parents=True, exist_ok=True)
         access_file_path.touch()
 
         Path(origin).mkdir(parents=True, exist_ok=True)
 
         plugin = SafeRunScriptPlugin()
-        args = SafeRunScriptPlugin.Args(origin=str(origin), script=f'cat {str(access_file)}')
+        args = SafeRunScriptPlugin.Args(
+            origin=str(origin),
+            allow_paths=allow_paths,
+            script=f"cat {str(access_file)}",
+        )
 
         if expected_exception:
             with pytest.raises(expected_exception):
